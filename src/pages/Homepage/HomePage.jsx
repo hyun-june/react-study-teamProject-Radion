bd10be882c217ec8f9bf665ea2832fc88051acf4
Hyunjun Lee <leehyunjune11@gmail.com>
2024-09-12 22:53:05 +0900

    [style] backgrounColor

--- src/pages/Homepage/HomePage.jsx
+++ src/pages/Homepage/HomePage.jsx
@@ -3,7 +3,7 @@ import Banner from './component/Banner/Banner'
 import MusicAlbum from './component/MusicAlbum/MusicAlbum';
 import MusicNewReleases from './component/MusicNewReleases/MusicNewReleases';
 import MusicPlaylist from './component/MusicPlaylist/MusicPlaylist';
-
+// import "./HomePage.style.css";
 
 const HomePage = () => {
   // const{
@@ -12,7 +12,7 @@ const HomePage = () => {
   // console.log("usePopularMusicQuery",data);
   return (
     <div>
-      <div className='hompage_container'>
+      <div className='homepage_container' style={{backgroundColor:"#1f1f1f"}}>
       홈페이지 입니다
       <Banner/>
       <MusicPlaylist/>
