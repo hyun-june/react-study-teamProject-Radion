5badccde3f6511ad05ed9ad0323dd3a1b432da26
kurotamashi30 <kurotamashi30@gmail.com>
2024-09-12 22:08:30 +0900

    hompage_dev_2

---
+++ src/hooks/useMusicAlbums.js
@@ -0,0 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+import api from '../utils/api';
+
+const fetchMusicAlbums = async () => {
+    try {
+        const response = await api.get('/recommendations', {
+            params: {
+                limit: 10,
+                market: 'KR',
+                local: 'ko_KR',
+                seed_artists: '4NHQUGzhtTLFvgF5SZesLK',
+                // seed_genres: 'classical,country',
+                seed_tracks: '0c6xIDDpzE81m2q797ordA',
+                // Add any other required parameters here
+            },
+        });
+        console.log("ddd", response.data); // Move this line above the return statement
+        return response.data;
+    } catch (error) {
+        throw new Error('Failed to fetch popular music');
+    }
+};
+
+export const useMusicAlbumsQuery = () => {
+    return useQuery({
+        queryKey: ['Music-popular'],
+        queryFn: fetchMusicAlbums,
+    });
+};
