b3581d54e4613f51923b3c97ce1c24d1d3ca504c
Hyun-june <leehyunjune11@naver.com>
2025-02-22 19:34:37 +0900

    pages

--- src/route/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-
-const PrivateRoute = ({ isAuthenticated }) => {
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
